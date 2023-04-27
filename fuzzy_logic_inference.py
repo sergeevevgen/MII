390d2a7cb3c7dd9bcca81007ed4d5438f1f580bc
Евгений Сергеев <alexevgenii@mail.ru>
2023-04-27 18:01:57 +0400

    а

--- fuzzy_logic_inference.py
+++ fuzzy_logic_inference.py
@@ -2,81 +2,71 @@ import matplotlib.pyplot as plt
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
-
 # Входные переменные
-distance = ctrl.Antecedent(np.arange(0, 1001, 1), 'distance')
-size = ctrl.Antecedent(np.arange(0, 11, 1), 'size')
-speed = ctrl.Antecedent(np.arange(0, 501, 1), 'speed')
+# Генерируем последовательности с (1 - начальное значение, 2 - конечное, 3 - шаг, 4 - название)
+distance = ctrl.Antecedent(np.arange(0, 121, 1), 'distance')
+speed = ctrl.Antecedent(np.arange(0, 110, 1), 'speed')
+
 
 # Выходная переменная
-danger = ctrl.Consequent(np.arange(0, 101, 1), 'danger')
+# Генерируем последовательности с (1 - начальное значение, 2 - конечное, 3 - шаг, 4 - название)
+volume = ctrl.Consequent(np.arange(0, 11, 1), 'volume')
 
 # Определение функций принадлежности
-
-distance['close'] = fuzz.trimf(distance.universe, [0, 0, 500])
-distance['medium'] = fuzz.trimf(distance.universe, [0, 500, 1000])
-distance['far'] = fuzz.trimf(distance.universe, [500, 1000, 1000])
-
-
-size['small'] = fuzz.trimf(size.universe, [0, 0, 5])
-size['medium'] = fuzz.trimf(size.universe, [0, 5, 10])
-size['large'] = fuzz.trimf(size.universe, [5, 10, 10])
+distance['short'] = fuzz.trimf(distance.universe, [0, 5, 20])
+distance['middle'] = fuzz.trimf(distance.universe, [15, 30, 70])
+distance['long'] = fuzz.trimf(distance.universe, [40, 80, 120])
+distance['far_far'] = fuzz.trimf(distance.universe, [100, 110, 200])
 
 
-speed['slow'] = fuzz.trimf(speed.universe, [0, 0, 250])
-speed['medium'] = fuzz.trimf(speed.universe, [0, 250, 500])
-speed['fast'] = fuzz.trimf(speed.universe, [250, 500, 500])
+speed['slow'] = fuzz.trimf(speed.universe, [0, 10, 25])
+speed['medium'] = fuzz.trimf(speed.universe, [15, 29, 50])
+speed['fast'] = fuzz.trimf(speed.universe, [45, 65, 109])
 
 
-danger['low'] = fuzz.trimf(danger.universe, [0, 0, 50])
-danger['medium'] = fuzz.trimf(danger.universe, [0, 50, 100])
-danger['high'] = fuzz.trimf(danger.universe, [50, 100, 100])
+volume['low'] = fuzz.trimf(volume.universe, [0, 4, 6])
+volume['medium'] = fuzz.trimf(volume.universe, [3, 5, 8])
+volume['high'] = fuzz.trimf(volume.universe, [6, 9, 10])
 
 distance.view()
-size.view()
 speed.view()
-danger.view()
+volume.view()
 
-# Определение правил
-rule1 = ctrl.Rule(distance['close'] & size['small'] & speed['slow'], danger['high'])
-rule2 = ctrl.Rule(distance['close'] & size['small'] & speed['medium'], danger['medium'])
-rule3 = ctrl.Rule(distance['close'] & size['small'] & speed['fast'], danger['low'])
-rule4 = ctrl.Rule(distance['close'] & size['medium'] & speed['slow'], danger['high'])
-rule5 = ctrl.Rule(distance['close'] & size['medium'] & speed['medium'], danger['medium'])
-rule6 = ctrl.Rule(distance['close'] & size['medium'] & speed['fast'], danger['low'])
-rule7 = ctrl.Rule(distance['close'] & size['large'] & speed['slow'], danger['high'])
-rule8 = ctrl.Rule(distance['close'] & size['large'] & speed['medium'], danger['high'])
-rule9 = ctrl.Rule(distance['close'] & size['large'] & speed['fast'], danger['medium'])
-rule10 = ctrl.Rule(distance['medium'] & size['small'] & speed['slow'], danger['medium'])
+# Формирование базы правил
+rule_1 = ctrl.Rule(distance['short'] & speed['slow'], volume['high'])
+rule_2 = ctrl.Rule(distance['short'] & speed['medium'], volume['medium'])
+rule_3 = ctrl.Rule(distance['short'] & speed['fast'], volume['high'])
+rule_4 = ctrl.Rule(distance['middle'] & speed['slow'], volume['high'])
+rule_5 = ctrl.Rule(distance['middle'] & speed['medium'], volume['low'])
+rule_6 = ctrl.Rule(distance['middle'] & speed['fast'], volume['medium'])
+rule_7 = ctrl.Rule(distance['long'] & speed['slow'], volume['high'])
+rule_8 = ctrl.Rule(distance['long'] & speed['medium'], volume['medium'])
+rule_9 = ctrl.Rule(distance['long'] & speed['fast'], volume['low'])
+rule_10 = ctrl.Rule(distance['far_far'] & speed['fast'], volume['low'])
 
-rule6.view()
+# rule_6.view()
 
 # Создание системы нечеткого вывода
-danger_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10])
-danger_simulation = ctrl.ControlSystemSimulation(danger_ctrl)
+volume_ctrl = ctrl.ControlSystem([rule_1, rule_2, rule_3, rule_4, rule_5, rule_6, rule_7, rule_8, rule_9, rule_10])
+volume_simulation = ctrl.ControlSystemSimulation(volume_ctrl)
 
 # Установка значений входных переменных
 Distance = 100
-Size = 10
-Speed = 350
+Speed = 40
 
-danger_simulation.input['distance'] = Distance
-danger_simulation.input['size'] = Size
-danger_simulation.input['speed'] = Speed
+volume_simulation.input['distance'] = Distance
+volume_simulation.input['speed'] = Speed
 
-# Выполнение расчетов
-danger_simulation.compute()
+# Аккумулирование заключений
+volume_simulation.compute()
 
-# Получение значения выходной переменной
-Danger = danger_simulation.output['danger']
+# Дефаззификация выходной переменной
+Volume = volume_simulation.output['volume']
 
-danger.view(sim=danger_simulation)
+volume.view(sim=volume_simulation)
 
-print('---Входные данные---')
-print(f'\nРасстояние до земли:  {Distance} км\n')
-print(f'Размер небесного тела:  {Size} м\n')
-print(f'Скорость движения к земле:  {Speed} км/ч\n')
-print('---Выходные данные---')
-print(f'\nСтепень опасности:  {round(Danger,2)}/100')
+print(f'Расстояние для преодоления:  {Distance} км')
+print(f'Скорость:  {Speed} км/ч')
+print(f'Объем топлива:  {round(Volume, 2)} л / км/ч ')
 
 plt.show()
